a070a993a821046843799b8c80737cbd7d93e11f
Brijesh <chavdabrijeshkumar25@gmail.com>
2022-10-15 21:01:51 +0530

    feat: add const keyword

---
+++ const.js
@@ -0,0 +1,13 @@
+// Topic: const keyword
+
+// # What is const?
+// - It is keyword which use to create variable.
+
+// # Why use const?
+// - When we create a variable using var and let we can change it's value.
+// - But when we create a variable using const we can't change it's value.
+// - Think as when you create a variable using const don't take as a you are creating a variable.
+// - Think as a you are creating CONSTANT (Which never change).
+
+// # How to use const?
+const pi = 3.14;

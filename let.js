86d7cc73f4a6e1a3a8db90cf095b97aab1a406f9
Brijesh <chavdabrijeshkumar25@gmail.com>
2022-10-15 20:54:28 +0530

    feat: add let keyword

---
+++ let.js
@@ -0,0 +1,20 @@
+// Topic: let
+
+// # What is let?
+// - It is keyword which you to create variable.
+
+// # Why use let?
+// - When we already var keyword to create a variable why we use let keyword.
+// - Because when you create variable use var it can be re-create again.
+// - For some case we have to avoid creating same variable twice too avoid the confusion.
+// - That why we use let keyword to create a variable and avoid duplication.
+
+
+// # How to use let?
+// We can use same variable name again with var.
+var name = "Brijesh";
+var name = "Hemil";
+
+// We can't use same variable name again with let.
+let name1 = "Brijesh";
+// let name1 = "Hemil"; // ERROR

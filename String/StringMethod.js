bba90125f316be22a77c4fd2759b15a989664162
Brijesh <chavdabrijeshkumar25@gmail.com>
2022-10-16 19:44:34 +0530

    feat: string index and method

---
+++ String/StringMethod.js
@@ -0,0 +1,25 @@
+// Topic: String Method
+
+// # trim
+var name = "   Brijesh   " 
+
+// NOTE:
+// - Don't use String method like this.
+// - You need to first save that variable.method(); in to variable then use it.
+// name.trim();
+// console.log(name);
+
+name = name.trim();
+console.log(name);
+
+// # toUpperCase
+name = name.toUpperCase();
+console.log(name);
+
+// # toLowerCase
+name = name.toLowerCase();
+console.log(name);
+
+// # slice
+name = name.slice(1,5);
+console.log(name);

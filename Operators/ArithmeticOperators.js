5b89dfdbe2377d9202a098638d91b95a50507028
Brijesh <chavdabrijeshkumar25@gmail.com>
2022-11-14 14:00:43 +0530

    refactor: arithmetic operators

--- Operators/ArithmeticOperators.js
+++ Operators/ArithmeticOperators.js
@@ -6,57 +6,23 @@
 
 // # How to use Arithmetic Operators?
 // ## List of Arithmetic Operators:
-// 1. + = Addition
-// 2. - = Subtraction
-// 3. * - Multiplication
-// 4. / = Division
-// 5. % = Remainder
-// 6. ** = Exponentiation
-// 7. ++ = Increment
-// 8. -- = Decrement
+// - + Addition
+// - - Subtraction
+// - * Multiplication
+// - / Division
+// - % Remainder
+// - ** Exponentiation
+// - ++ Increment
+// - -- Decrement
 
-// 1. + = Addition
-var a = 1;
-var b = 2;
-var c = a + b;
-console.log(c);
+let a = 5;
+let b = 2;
 
-// 2. - = Subtraction
-var a = 1;
-var b = 2;
-var c = a - b;
-console.log(c);
-
-// 3. * = Multiplication
-var a = 1;
-var b = 2;
-var c = a * b;
-console.log(c);
-
-// 4. / = Division
-var a = 1;
-var b = 2;
-var c = a / b;
-console.log(c);
-
-// 5. % = Remainder
-var a = 1;
-var b = 2;
-var c = a % b;
-console.log(c);
-
-// 6. ** = Exponentiation 
-var a = 1;
-var b = 2;
-var c = a ** b; // 1^2
-console.log(c);
-
-// 7. ++ = Increment
-var a = 1;
-a++;
-console.log(a);
-
-// 8. -- = Decrement
-var a = 1;
-a--;
-console.log(a);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+console.log(a++);
+console.log(a--);

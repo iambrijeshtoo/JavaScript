eea77472f49821659aefde16aafcb5268c96f271
Brijesh <chavdabrijeshkumar25@gmail.com>
2022-11-20 20:59:49 +0530

    feat: add do while loop

---
+++ Loop/DoWhile.js
@@ -0,0 +1,18 @@
+// Do While Loop
+
+/* 
+    # What is Do While Loop?
+    - This loop is same as while loop but it will execute the code at least one time.
+    - Even if the condition is true or false.
+
+    # Syntax:
+    do {
+        Code...
+    } while (condition);
+*/
+
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);

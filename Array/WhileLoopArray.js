1f4ab50ba744b6fe8233313d04c77078fabeac06
Brijesh <chavdabrijeshkumar25@gmail.com>
2022-11-22 21:28:52 +0530

    feat: add while loop in array

---
+++ Array/WhileLoopArray.js
@@ -0,0 +1,10 @@
+// While Loop In Array
+
+const array = ["Item1", "Item2", "Item3"];
+
+let index = 0;
+while (index < array.length) {
+    const element = array[index];
+    console.log(element);
+  index++;
+}

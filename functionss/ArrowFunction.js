0862963c0ea3886a669021662111e1b144023750
Brijesh <chavdabrijeshkumar25@gmail.com>
2022-11-30 19:25:38 +0530

    refactor: arrow function

--- functionss/ArrowFunction.js
+++ functionss/ArrowFunction.js
@@ -1,36 +1,15 @@
 // Arrow Function
 
-/* 
-    # Default function
-    ## Syntax:
-    function functionName (parameter1, parameter2){
-        Code...
-    return returnValue;
-}
-*/
-function sum(number1, number2) {
-  return console.log(number1 + number2);
-}
-
-/*
-    # Arrow function
-    ## Syntax:
-    datatype variableName = () => {Code}
-*/
-const greetings = () => {
+// Arrow function
+const greet = () => {
   console.log("Hello, World!");
 };
 
-/* 
-  ## Syntax: When you have one parameter to pass (Remove small brackets).
-  datatype functionName = parameter1 => { return Code... }
-*/
-const greetingsCustomName = (name) => {
+// For single parameter to pass in function you can remove small bracket.
+const greetWithCustomName = (name) => {
   return console.log(`Hello ${name}`);
 };
 
-/* 
-  # Syntax: When your function return something in one single line (Remove the return keyword).
-  datatype functionName = parameter1 =>  Code... }
-*/
-const studentName = (studentName) => console.log(`Hello ${studentName}`);
+// When function returns something in single line remove return keyword.
+const greetWithStudentName = (studentName) =>
+  console.log(`Hello ${studentName}`);

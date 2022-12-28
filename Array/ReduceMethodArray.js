6ac181d40033da49999f951f3252d2bcf3e0c89d
Brijesh <chavdabrijeshkumar25@gmail.com>
2022-12-28 20:15:49 +0530

    feat: add note for reduce method in array

--- Array/ReduceMethodArray.js
+++ Array/ReduceMethodArray.js
@@ -1,21 +1,18 @@
 // Reduce method in array
 
-/* 
-    # What is reduce method?
-    -> Before understanding the "reduce method" you need to understand what "reduce" word means. It means that something that can be shorten or smaller.
+/*
+  # What is reduce method?
+  -> Before understanding the "reduce method" you need to understand what "reduce" word means. It means that something that can be shorten or smaller.
 
-    # Why use reduce method?
-    -> When you use reduce method on array it will give you back the sum of the all the elements inside the array.
+  # Why use reduce method?
+  -> When you use reduce method on array it will give you back the sum of the all the elements inside the array.
 
-    # How to use reduce method?
-    -> It's take callback function as parameter and default value.
+  # How to use reduce method?
+  -> It's take callback function as parameter and default value.
 
-    # NOTE:
-    -> If you pass default value 100 it will return 115. Because it taking default value 100 and adding the sum of all the elements and returning the total.
+  ## Syntax:
 
-    ## Syntax:
-
-    array.reduce(()=>{}, defaultValue);
+  array.reduce(()=>{}, defaultValue);
 */
 
 const array = [1, 2, 3, 4, 5];
@@ -26,3 +23,19 @@ const total = array.reduce(
 );
 
 console.log(total);
+
+/*
+  # NOTE:
+  -> If you pass default value 100 it will return 115. Because it taking default value 100 and adding the sum of all the elements and returning the total.
+  -> Don't confuse the reduce method with fibonacci series. They both are different. In fibonacci series it pick the last to digits and give back the sum. But in reduce method it actually pick the next element.
+
+  Fibonacci series
+  1 + 1 = 1
+  1 + 2 = 3
+  2 + 3 = 5
+
+  Reduce method
+  1 + 2 = 3
+  3 + 3 = 6
+  6 + 4 = 10
+*/

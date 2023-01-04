aa3b7d05b10508685d8fd19eaf3e5b8e807594e0
Brijesh <chavdabrijeshkumar25@gmail.com>
2023-01-05 05:15:58 +0530

    feat: add bind

--- CallApplyBind/Apply.js
+++ CallApplyBind/Apply.js
@@ -9,7 +9,7 @@
     - Internally apply is using call.
 */
 
-function usingApply(objectName) {
+function usingBind(objectName) {
   console.log(this.name1);
   console.log(objectName);
   console.log("Hello, World!");
@@ -28,5 +28,5 @@ const object2 = {
 };
 
 // usingApply.call(); === usingApply();
-usingApply.call(object1, ["I am object1"]); // (objectName for this, argument)
-usingApply.call(object2, ["I am object2"]); // Calling object 1 method for object 2
+usingBind.call(object1, ["I am object1"]); // (objectName for this, argument)
+usingBind.call(object2, ["I am object2"]); 
---
+++ CallApplyBind/Bind.js
@@ -0,0 +1,33 @@
+// Topic : Bind
+
+/* 
+    # What is Bind?
+    - It works same as call but it will return function.
+
+    # Note
+    - You need to store bind(); in variable in order to access it.
+*/
+
+function usingBind(objectName) {
+  console.log(this.name1);
+  console.log(objectName);
+  console.log("Hello, World!");
+}
+
+const object1 = {
+  name1: "Value1",
+  name2: "Value2",
+  name3: "Value3",
+};
+
+const object2 = {
+  name1: "Value1",
+  name2: "Value2",
+  name3: "Value3",
+};
+
+const function1 = usingBind.bind(object1, ["I am object1"]); // (objectName for this, argument)
+function1();
+
+const function2 = usingBind.bind(object2, "I am object2");
+function2();

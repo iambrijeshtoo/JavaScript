da94a023f662d719fc8b86e06572d9add6bc8a67
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2023-05-16 17:52:43 +0530

    refactor: datatype

---
+++ 4Datatype/2ConceptOfDatatype.js
@@ -0,0 +1,9 @@
+// Concept of Datatype
+
+// Adding string with number JavaScript treat number as a string.
+let var1 = "string" + 1 + 2;
+console.log(var1);
+
+// JavaScript evaluate code from left to right so it will do the arithmetic operation first than treat the answer as a string.
+let var2 = 1 + 2 + "string";
+console.log(var2);
--- 4_datatype/2_concept_of_datatype.js
+++
@@ -1,7 +0,0 @@
-// Adding string with number JavaScript treat number as a string.
-let a = "abc" + 1 + 2;
-console.log(a);
-
-// JavaScript evaluate code from left to right so it will do the arithmetic operation first than treat the answer as a string. 
-let b = 1 + 2 + "abc";
-console.log(b);

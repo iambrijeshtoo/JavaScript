f74e4c629f867405b8b24a70010ff817a49f8056
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2023-05-26 17:00:48 +0530

    feat: add charAt method

---
+++ 10String/StringMethod/ChatAt.js
@@ -0,0 +1,11 @@
+// charAt() Method
+
+// Q. What is charAt()?
+// - The charAt() method returns the character at a specified index (position) in a string.
+
+// SYNTAX
+// variable.charAt(index);
+
+let str = "Hello world";
+let char = str.charAt(3);
+console.log(char);

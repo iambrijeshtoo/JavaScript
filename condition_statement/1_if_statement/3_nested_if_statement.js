b313902f98fd77dc6a1d3f54bf78a2da2d7dd43a
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2023-07-01 20:47:13 +0530

    refactor: if statement

--- condition_statement/1_if_statement/3_nested_if_statement.js
+++ condition_statement/1_if_statement/3_nested_if_statement.js
@@ -6,9 +6,5 @@ if (age >= 18) {
 
   if (hasID) {
     console.log("You have a valid ID. Enjoy the event!");
-  } else {
-    console.log("Please bring a valid ID for entry.");
   }
-} else {
-  console.log("You are not old enough to enter the venue.");
 }

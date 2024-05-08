8ca10d5784d6724fc0a981738141a230c885b0bd
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2024-05-08 14:23:59 +0530

    refactor: directive

---
+++ 03_directive/01_use_strict.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// WHAT
+// - "Use strict" is a directive in JavaScript that activates strict mode, a stricter set of rules for interpreting and executing JavaScript code.
+
+// WHY
+// - It's used to catch common coding errors, enforce better coding practices, and enhance security by disallowing potentially unsafe features and behaviors.
+
+// HOW
+// - To activate strict mode, you include the string literal "use strict"; at the beginning of a script or a function. This directive tells the JavaScript engine to interpret the code that follows it in strict mode.
+
+// PROS
+// - Helps catch common coding errors by generating more errors in certain situations.
+// - Enforces better coding practices by eliminating ambiguous or problematic syntax.
+// - Enhances security by disallowing potentially dangerous features and behaviors.
+
+// CONS
+// - Strict mode may break existing code that relies on non-strict behavior, requiring modifications for compatibility.
+// - Some developers may find strict mode too restrictive for certain use cases, leading to additional overhead in code development and maintenance.

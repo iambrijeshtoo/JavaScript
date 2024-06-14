31bb02eb951526f539f311e67bb937c2145c65ff
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2024-06-14 11:06:03 +0530

    feat: truthy and falsy

--- 17_truthy_and_falsy/truthy_and_falsy.js
+++ 17_truthy_and_falsy/truthy_and_falsy.js
@@ -1,26 +1,26 @@
-// Falsy value
+// Truthy VS Falsy
+
+// Falsy value: Any value which is treated as a false. Known as a falsy value.
 // false
 // 0
 // -0
 // BigInt 0n
-// ""
+// Empty string: ""
 // null
 // undefined
 // NaN
 
-// Truthy value
-// - Any value other than falsy.
+if (0) {
+  console.log("code executed");
+}
+
+// Truthy value: Any value other than falsy.
 // "0"
 // "false"
-// " "
+// String with white space: "    "
 // []
 // {}
 // function(){}
-
-// For CP and Interview
-// false == 0 -> true
-// false == "" -> true
-// 0 == "" -> true
 
 const emptyArray = [];
 if (emptyArray.length === 0) {
@@ -31,3 +31,16 @@ const emptyObject = {};
 if (Object.keys(emptyObject).length === 0) {
   console.log("Objet is empty");
 }
+
+// For Interview
+// false == 0 -> true
+// false == -0 -> true
+// false == 0n -> true
+// false == "0" -> true
+// false == "" -> true
+
+// false == null -> false
+// false == undefine -> false
+// false == NaN -> false
+const result = false == NaN;
+console.log(result);
--- code_test_space.js
+++ code_test_space.js
@@ -1,4 +1,4 @@
-// TODO: This example is connected to the closer concept which is the video number - 49. 
+// TODO: This example is connected to the closer concept which is the video number - 49.
 // function outerRegularFunction() {
 //   const variable = "value";
 //   function innerRegularFunction() {
@@ -7,3 +7,8 @@
 //   console.log(innerRegularFunction());
 // }
 // console.log(outerRegularFunction());
+
+const num = 1
+if (num) {
+    console.log("hey");
+}
